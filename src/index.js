6d69e91441ffb8b91e1fb29fa62838c0db706921
Yago <yagosantoslima81@gmail.com>
2025-07-15 03:26:34 -0300

    mudei o index princial pra abrir as portas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-const {connect} = require("./connect");
-const {load} = require("./load")
+const { connect } = require("./connect");
+const { load } = require("./load");
+const express = require("express");
 
-async function start(){
+async function start() {
   const socket = await connect();
+  load(socket);
 
-  load(socket)
+  // 🟢 Mantém a porta aberta pra hospedagem detectar
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.get("/", (req, res) => res.send("Bot WhatsApp rodando!"));
+  app.listen(port, () => console.log(`Servidor ativo na porta ${port}`));
 }
-start()
+start();
